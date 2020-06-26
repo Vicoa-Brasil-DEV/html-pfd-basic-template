1bf957ccda0a3bfd06f32bae29862cc8c2195cf6
Antônio Bernardino da Silva <antoniobsilvaprof@hotmail.com>
2020-06-25 22:29:26 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const pdf = require('html-pdf')
 const ejs = require('ejs');
 
+// Instancia o Express
 const app = express();
 
-
+// Autoriza o Express a usar Json
 app.use(express.json())
 
+
+// Essa rota cria o arquivo
 app.get('/', (request, response) => {
 
   ejs.renderFile('./templates/index.ejs', { name: 'Acho que deu certo' }, (err, html) => {
@@ -14,10 +17,11 @@ app.get('/', (request, response) => {
       return response.status(500).json({ message: 'Error in server!' })
     }
 
+    // Opções que posso definir no pdf
     const options = {
       format: 'A4',
       border: {
-        rigth: '8'
+        rigth: '10'
       }
     };
     pdf.create(html, options).toFile('./uploads/report.pdf', (error, res) => {
@@ -29,6 +33,7 @@ app.get('/', (request, response) => {
     });
   });
 
+  //Essa rota serve para disponibilizar o arquivo
   app.get('/donwload', (request, response) => {
     response.type('pdf');
     response.download('./uploads/report.pdf');
